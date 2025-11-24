42994f856cb23cee5f37413124f89d38cb2e04d1
vaclav <vaclav.krivka07@gmail.com>
2025-11-24 14:15:34 +0100

    made it producent consumer

--- cracker.py
+++ cracker.py
@@ -1,17 +1,13 @@
 import hashlib
 import multiprocessing
 import string
-
-
+import time
 
 
 lower_case_alphabet = list(string.ascii_lowercase)
 upper_case_alphabet = list(string.ascii_uppercase)
 numbers = list(string.digits)
 special_characters = list(string.punctuation)
-
-
-
 
 
 class PasswordCracker:
@@ -66,56 +62,67 @@ class PasswordCracker:
 
 
 
-    def worker_crack_password(self, symbols_count, start_index, end_index):
-        password_index_list = [0] * symbols_count
-        print(password_index_list)
-        counter = 0
-        password_index_list[0] = start_index
-        while (password_index_list is not False and password_index_list[0] != end_index) and self.finish_flag.value == 0:
-            counter += 1
-            if counter >= 300000:
-                counter = 0
-                print(self.get_password_from_index_list(password_index_list) + "////start:" +  str(self.final_char_set[start_index]) + "/end:" + str(self.final_char_set[end_index-1]))
-            current_password = self.get_password_from_index_list(password_index_list)
-            if hashlib.sha384(current_password.encode('utf-8')).hexdigest() == self.hash_target:
-                # Ulozeni do sdilene pameti (musime prevest na bytes)
-                self.final_password.value = current_password.encode('utf-8')
-                self.finish_flag.value = 1 #True
-                return self.get_password_from_index_list(password_index_list)
-            password_index_list = self.increment_index_list(password_index_list)
+    def worker_crack_password(self, task_queue):
+        #v manageru je queue.put((symbols_count, start, end))
+
+        while True:
+            tasks = task_queue.get()
+            if self.finish_flag.value == 1 or tasks is None:
+                break
+            password_index_list = [0] * tasks[0]
+
+            print(password_index_list)
+            counter = 0
+            password_index_list[0] = tasks[1]
+            while (password_index_list is not False and password_index_list[0] != tasks[2]) and self.finish_flag.value == 0:
+                counter += 1
+                if counter >= 300000:
+                    counter = 0
+                    print(self.get_password_from_index_list(password_index_list) + "////start:" +  str(self.final_char_set[tasks[1]]) + "/end:" + str(self.final_char_set[tasks[2]-1]))
+                current_password = self.get_password_from_index_list(password_index_list)
+                if hashlib.sha384(current_password.encode('utf-8')).hexdigest() == self.hash_target:
+                    # Ulozeni do sdilene pameti (musime prevest na bytes)
+                    self.final_password.value = current_password.encode('utf-8')
+                    self.finish_flag.value = 1 #True
+                    return self.get_password_from_index_list(password_index_list)
+                password_index_list = self.increment_index_list(password_index_list)
         #password not found
         return None
 
 
 
 
-    def process_manager(self, max_chars, max_processes):
+    def process_manager(self, max_chars, max_processes, task_queue):
 
-        password_index_list = []
+
         base, rem = divmod(len(self.final_char_set), max_processes)
         self.finish_flag.value = 0
-
         processes = []
+
+        for _ in range(max_processes):
+            p = multiprocessing.Process(target=self.worker_crack_password, args=(task_queue,))
+            p.start()
+            processes.append(p)
 
         for symbols_count in range(1, max_chars + 1):
             if self.finish_flag.value == 1:
                 break
 
-            password_index_list.append(0)
+
             for process in range(0, max_processes):
                 #genialni matika od chata, tohle bych fakt nevymyslel TBH
-                start = process * base + min(process, rem)
+                current_process_start = process * base + min(process, rem)
                 size = base + (1 if process < rem else 0)
                 #konec genialni matickyy
-                end = start + size
-                password_index_list[0] = start
+                current_process_end = current_process_start + size
+                task_queue.put((symbols_count , current_process_start, current_process_end))
 
-                p = multiprocessing.Process(target=self.worker_crack_password, args=(symbols_count, start, end))
-                p.start()
-                processes.append(p)
-            for pro in processes:
-                pro.join()
-            processes.clear()
+
+        for _ in range(max_processes):
+            task_queue.put(None)  # Pekne jedovata pill
+
+        for process in processes:
+            process.join()
 
         if self.final_password != "":
             print(f"Ulozene heslo je: {self.final_password.value.decode('utf-8')}")
@@ -125,10 +132,17 @@ class PasswordCracker:
 
 
 if __name__ == "__main__":
-    password_hash = "aeeff5df4d122b5926acd916bea10fbc9362bd21b16fe049a2b242d4e6a2af93e7974358da58ee31bdc17e12bd68a19a"
 
+    password_hash = "9e0133f2a137e8eb48b7f27c25f06a7f4f9a3410b045bfe6823246608d1ee827e31e38bde7cfdfcb8702741b60449a3a"
+    #ahojj
 
-
-
+    queue = multiprocessing.Queue()
+    start = time.time()
     Cracker = PasswordCracker(target_hash=password_hash)
-    Cracker.process_manager(9, 8)
+    p1 = multiprocessing.Process(target=Cracker.process_manager, args=(6, 4, queue,))
+    p1.start()
+    p1.join()
+
+
+    end = time.time()
+    print("Vypocet bez trval {:.6f} sec.".format((end - start)))
